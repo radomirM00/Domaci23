94e3a6b43554a811fe1f8a5eb28c27f81d3239bc
radomirM00 <mlvcrdmr@gmail.com>
2022-09-25 18:05:15 +0200

    Domaci23

---
+++ src/main/java/Base.java
@@ -0,0 +1,62 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class Base {
+    private static WebDriver driver;
+    public Base (String url){
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    public static void closeRegistrationModal(String element){
+        WebElement xBtn = driver.findElement(By.className(element));
+        xBtn.click();
+    }
+    public static void selectLink(String path){
+        WebElement selection = driver.findElement(By.xpath(path));
+        selection.click();
+    }
+
+    public static void scrollAndSelectLink(String path){
+        WebElement link = driver.findElement(By.xpath(path));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(link);
+        link.click();
+    }
+
+    public static List<WebElement> links(String path){
+        List<WebElement> list = driver.findElements(By.xpath(path));
+        return list;
+    }
+
+    public static WebElement filterLinks(List<WebElement> links,String href) {
+
+        WebElement link = null;
+        for (WebElement we : links) {
+            if (we.getAttribute("href").equals(href)) {
+                link = we;
+            }
+        }
+        return link;
+    }
+
+    public static void printCategoryTitles(){
+        WebElement categoriesWrapper = driver.findElement(By.id("realCategoriesHolder"));
+        List<WebElement> categoryTitles = categoriesWrapper.findElements(By.className("categoryTitle"));
+        for (WebElement we:categoryTitles){
+            System.out.println(we.findElement(By.tagName("span")).getText());
+        }
+    }
+
+    public static void closeDriver(){
+        driver.close();
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main extends Base{
+    public Main(){
+        super("https://www.kupujemprodajem.com/index.php");
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Main test = new Main();
+        test.closeRegistrationModal("kpBoxCloseButton");
+        ///html/body/div[2]/div/div[1]/div[2]/div[4]
+        test.selectLink("//*[@id=\"cookieConsentHolder\"]/div/div/div[2]/input");
+        test.filterLinks(links("html/body/div[2]/div/div[1]/div[2]/div[4]/a"),"https://www.kupujemprodajem.com/bicikli/kategorija/912").click();
+        test.printCategoryTitles();
+        test.scrollAndSelectLink("//*[@id=\"groupBox1360\"]/div[1]/h2/a/span");
+        Thread.sleep(5000);
+        test.closeDriver();
+    }
+}
